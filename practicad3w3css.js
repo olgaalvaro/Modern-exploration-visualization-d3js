0d72769e4f49f7c286f2cf76a427de02659ede30
olgaalvaro <58437991+olgaalvaro@users.noreply.github.com>
2020-03-06 00:40:54 +0100

    Add files via upload

--- practicad3w3css.js
+++ practicad3w3css.js
@@ -5,10 +5,6 @@ const heightCabecera = 60;
 const posxCabecera = 10;
 const posyCabecera = 40;
 
-const input = [5, 10, 15, 20];
-const maxInput = d3.max(input);
-const rectWidth = 20;
-
 const svg = d3.select('#map')
    .append('svg');
 
@@ -32,7 +28,7 @@ const urljson = 'https://gist.githubusercontent.com/miguepiscy/2d431ec3bc101ef62
 // Cargar el csv
 const urlcsv = 'https://gist.githubusercontent.com/olgaalvaro/915a0740f47d75e40f9fc3ef28943ec1/raw/1d772883a16a211d40079080b9ce9261cb249978/airbnbmadridbyneighbourhood';
 
-
+// Json y Csv
 d3.json(urljson).then((featureCollection) => {
   	d3.csv(urlcsv).then(function (data) {
         data.forEach(function (d) {
@@ -42,7 +38,7 @@ d3.json(urljson).then((featureCollection) => {
             //console.log(d);
             return d;
         })
-        console.log(data[0]);
+        // Función pinta el mapa de Madrid
         drawMap(featureCollection,data, groupMap);
     })
     
@@ -51,29 +47,30 @@ d3.json(urljson).then((featureCollection) => {
     		throw error;
 });
 
+// Mostrar la leyenda del barrio seleccionado
 function drawLeyend(name, price) {
  
 
   const descCabecera = `Barrio: ${name} (Precio: ${price})`;
 
   const groupCabecera = svg.append('g').attr('class', 'cabecera');
+
   const rectCabecera = groupCabecera
     .append('rect');
+
 
   rectCabecera
     .attr('x', 0)
     .attr('y', posyCabecera)
-    .attr('width', width/2)
+    .attr('width', width)
     .attr('height', heightCabecera)
     .attr('fill', 'white');
 
- 
-
+  
   const textCabecera = groupCabecera.append('text');
   textCabecera
     .attr('x', posxCabecera)
     .attr('y', posyCabecera + heightCabecera/2)
-    .attr('fill', 'purple')
     .text(d => descCabecera);
 }
 
@@ -92,97 +89,12 @@ function mouseleaveNeighbourhood(d, i){
       .duration(200)
       .attr('fill', (d) => color(d.properties.avgprice));
 }
-function drawGraph(barrio, groupGraph,data)
+
+// Pintar el char bart con la información del total viviendas por nº de habitaciones
+// según el barrio seleccionado
+function drawGraph(barrio, groupGraph,datagraph)
 {
   console.log('drawGraph')
-  console.log(data);
-  console.log(barrio)
-
-  const width = 400;
-  const height = 400;
-
-  groupGraph.attr('width', width)
-    .attr('height', height);
-
-
-  const xscale = d3.scaleLinear()
-    .domain([0, d3.max(input)])
-    .range([0, width ]);
-
-  const yscale = d3.scaleLinear()
-    .domain([0, d3.max(input)])
-    .range([height, 0]);
-
-  const x_axis = d3.axisBottom()
-    .scale(xscale);
-
-  const y_axis = d3.axisLeft()
-    .scale(yscale);  
-
-  groupGraph.append("g")
-     .attr("transform", "translate(50, 10)")
-     .call(y_axis);
-
-  const xAxisTranslate = height + 10;
-
-  groupGraph.append("g")
-    .attr("transform", "translate(50, " + xAxisTranslate  +")")
-    .call(x_axis);
-      
-
-  const rect = groupGraph.selectAll('rect')
-    .data(input)
-    .enter()
-    .append('rect');
-
-  rect.attr('class', (d)=> {
-    if (d > 10){
-      return 'rectwarning';
-    }
-  })
-
-  rect
-    .attr('x', posX)
-    .attr('y', (d) => height - scale(d))
-    .attr('width', rectWidth)
-    .attr('height', scale);  // attr('height', (d) => scale(d))
-
-
-  const text = groupGraph
-    .selectAll('text')
-    .data(input)
-    .enter()
-    .append('text');
-
-  text.text(d=>d)
-    .attr('x', posX)
-    .attr('y', (d) => posY(d) - 25 ); // para subir se resta
-
-  text.attr('class', (d) =>{
-    if (d > 10){
-      return 'text rectwarning';
-    }
-    return 'text';
-  })
-
-}
-
-function scale(d) {
-  const scaleNum = (height - 20) / maxInput;
-  return scaleNum * d;
-}
-
-function posX(d, index) {
-  return index * (rectWidth + 1);
-}
-
-function posY(d) {
-  return height - scale(d);
-}
-
-function drawGraph1(barrio, groupGraph,datagraph)
-{
-  console.log('drawGraph1')
   console.log(datagraph);
   console.log(barrio)
 
@@ -193,13 +105,19 @@ function drawGraph1(barrio, groupGraph,datagraph)
     .remove()
     .exit();
 
+  groupGraph.selectAll('text')
+    .remove()
+    .exit();
+
+
   groupGraph.attr('width', widthgraph)
     .attr('height', heightgraph)
-    .attr('transform', `translate(100, 50)`);
+    .attr('transform', `translate(100, 100)`);
 
   const xScale = d3.scaleBand()
     .domain(datagraph.map(function(d){return d.bedrooms;}))
-    .range([0, widthgraph]);
+    .range([0, widthgraph])
+    .paddingInner(0.05);
 
    
   const xAxis = groupGraph.append('g')
@@ -209,7 +127,6 @@ function drawGraph1(barrio, groupGraph,datagraph)
 
   
   const yScale = d3.scaleLinear()
-    //.domain([0,d3.max(datagraph, d => d3.max(d, +d.total))])
     .domain([0,d3.max(datagraph,function (d) { return +d.total}) ])
     .range([heightgraph,0]);
 
@@ -223,9 +140,9 @@ function drawGraph1(barrio, groupGraph,datagraph)
     .remove()
     .exit()
     .data(datagraph);
-
   
   barchart
+    .data(datagraph)
     .enter()
     .append('rect')
     .attr('class', 'bar')
@@ -233,58 +150,62 @@ function drawGraph1(barrio, groupGraph,datagraph)
     .attr('y', function(d) { return yScale(+d.total); })
     .attr('width', xScale.bandwidth())
     .attr('height', function(d) { return heightgraph - yScale(+d.total); });
-    //.attr('transform', `translate(0, ${heightgraph})`);
+/*
+  barchart.append('g')
+    .attr('class', 'grid')
+    .attr('transform', `translate(0, ${heightgraph })`)
+    .call(d3.axisBottom()
+        .scale(xScale)
+        .tickSize(-heightgraph , 0, 0)
+        .tickFormat(''));
+
+  barchart.append('g')
+    .attr('class', 'grid')
+    .call(d3.axisLeft()
+        .scale(yScale)
+        .tickSize(-widthgraph , 0, 0)
+        .tickFormat(''));
+
+        https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+
+*/
+
+  const labels = groupGraph.selectAll('.labelbar')    
+    .data(datagraph)
+    .enter()
+    .append('text')
+    .attr('class','labelbar')
+    .attr('x', function(d) { return xScale(d.bedrooms) + (xScale.bandwidth()/2); })
+    .attr('y', function(d) { return yScale(+d.total) -10 ; })
+    .text(function(d) { return '  ' +d.total; });     
+
 
   groupGraph.append('text')
-   .attr('transform', 'translate(100,0)')
-   .attr('x', 50)
-   .attr('y', 50)
-   .attr('font-size', '24px')
-   .text('Texto Prueba');
+   .attr('class', 'titlebarchart')
+   .attr('x', 0)
+   .attr('y', 0)
+   .attr('transform', 'translate(0,-50)')
+   .text(`Bar Chart: ${barrio}`);
 
   groupGraph.append('g')
-   .attr('transform', `translate(300, ${heightgraph +50} )`)
-   //.call(d3.axisBottom(xScale))
+   .attr('class', 'xtitlebc')
+   .attr('transform', `translate(300, ${heightgraph +40} )`)
    .append('text')
-   //.attr('y', heightgraph  - 250)
-   //.attr('x', widthgraph  - 200)
-   .attr('text-anchor', 'end')
-   .attr('stroke', 'black')
-   .text('N Habitaciones');
+   .text('Nº Habitaciones');
    
   groupGraph.append('g')
-   //.call(d3.axisLeft(yScale)
-   //.tickFormat(function(d){
-   //    return + d;
-   //}).ticks(10))
+   .attr('class', 'ytitlebc')
    .attr('transform', `translate(0, ${heightgraph/3} )`)
    .append('text')
    .attr('transform', 'rotate(-90)')
    .attr('y', 6)
    .attr('dy', '-5.1em')
-   .attr('text-anchor', 'end')
-   .attr('stroke', 'black')
    .text('Nº Viviendas');
-
-/*
-  console.log('d.bedrooms');
-  console.log(datagraph.bedrooms);
-  rects.selectAll("rect")
-    .data(datagraph)
-    .enter()
-    .append('rect')
-    .attr('x', d => xScale(xValue(d)))
-    .attr('y', d=> yScale(yValue(d)))
-*/
-    
-    //.attr("height", d=> yScale(d[0]) - yScale(d[1]))
-    //.attr("width", xScale.bandwidth())  
-
 
 }
 
 
-
+// Pintar el mapa de Madrid 
 function drawMap(featureCollection,data, gmap) { 
 
 	// Proyección
@@ -292,6 +213,7 @@ function drawMap(featureCollection,data, gmap) {
     const projection = d3.geoMercator()
        .fitSize([width-100, height-100], featureCollection)
        .center(center)
+
        .translate([width/2, height/2 + 65]);
        //.translate([width/4, height/2 - 50]);		// Punto central del mapa   
 
@@ -305,7 +227,9 @@ function drawMap(featureCollection,data, gmap) {
     const neighbourhoodsPath = gmap.selectAll('.neighbourhoods')
     	.data(features)
     	.enter()
-    	.append('path');
+    	.append('path')
+      .attr('stroke', 'black')
+      .attr('stroke-width', 0.5);
 
     neighbourhoodsPath.attr('d', pathProjection);
 
@@ -314,42 +238,31 @@ function drawMap(featureCollection,data, gmap) {
     	.on('mouseleave', mouseleaveNeighbourhood);
 
     function clickNeighbourhood(d, i) {
-        //d.opacity = d.opacity ? 0 : 1;
-      //d3.select(this).attr('opacity', d.opacity);
-      
-      console.log(d.properties.name);
+      // Pintar la leyenda sobre el mapa
       drawLeyend(d.properties.name, d.properties.avgprice);
-      console.log('1-----------------');
-      console.log(data);
 
+      // Filtrar los datos del csv según el barrio seleccionado
       const neighbourselect = d.properties.name;
-      /*
-      const datagraph = data.filter(function(d){
-        console.log('2-----' + d.neighbourhood + ' ' + neighbourselect);
-        return d.neighbourhood == neighbourselect;}*/
       const datagraph = data.filter(
-        //datos => datos.neighbourhood ==barrioSeleccionado
         datos => datos.neighbourhood == neighbourselect
       );
 
-      //console.log('filter');
-      //console.log(neighbourselect);
-      //console.log(datagraph);
-      drawGraph1(d.properties.name, groupGraph,datagraph);
-      //drawGraph(d.properties.name, groupGraph,datagraph);
+      // Pintar el bar chart con los datos del barrio seleccionado
+      drawGraph(d.properties.name, groupGraph,datagraph);
       
    }
-    // Color al mapa por barrio
+  
+  // Color al mapa por barrio
 	// Escala lineal con 6 rectangulos en base al max del precio medio por barrio
 	const numberOfLegends = 6;
     color = d3.scaleQuantize([0, maxavgprice], d3.schemeBlues[numberOfLegends])
     neighbourhoodsPath.attr('fill', (d) => color(d.properties.avgprice))
-    // Leyenda con el precio (rectángulo)
+  
+  // Leyenda con el precio (rectángulo)
 	const legend = gmap.append('g')
 	  .attr('class', 'legend')
 	  .attr('title', 'AVG Price');
 
-	// d3.legendcolor
 
 	// Escala lineal con 6 rectangulos en base al max del precio medio por barrio
 	const scaleLegend = d3.scaleLinear()
@@ -363,7 +276,6 @@ function drawMap(featureCollection,data, gmap) {
 			   .append('g')
 			   .attr('transform', `translate (${posX}, 10) `);
 
-		   // console.log(index);	   	
 		   const rectColor = legendGroup
 			   .append('rect');
 
